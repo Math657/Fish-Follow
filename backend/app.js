f6f84cde3eebd056a962ebc2f11dbe6e06c66c33
Mathieu Dessaint <mathieu.dessaint10@gmail.com>
2021-02-09 18:08:54 +0100

    back -> backend

--- backend/app.js
+++ backend/app.js
@@ -13,11 +13,9 @@ const app = express()
 const path = require('path')
 
 // app.use((req, res, next) => {
-//     // res.setHeader('Access-Control-Allow-Origin', 'https://fishnfollow.com') // prod
 //     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080') // local
 //     res.setHeader('Access-Control-Allow-Credentials', true)
 //     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, Bearer')
-//     // res.setHeader('Access-Control-Allow-Headers', '*')
 //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
 //     next()
 // })
